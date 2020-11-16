efeecf7258f4e8a25818f76369184b51de8df74b
Koushik <koushik1@users.noreply.github.com>
2020-11-16 15:14:56 -0500

    Create lock.h

---
+++ csc501-lab3/h/lock.h
@@ -0,0 +1,24 @@
+#ifndef _LOCK_H_
+#define _LOCK_H_
+
+#define READ	1
+#define WRITE	2
+#define DELETED -2
+#define NLOCKS 50
+
+#define LFREE  0
+#define LUSED  1
+
+struct	lentry	{		
+	char lstate;		
+	int	lqhead;		
+	int	lqtail;		
+	int	ltype;	
+	int	lprio;		
+	int lproc_list[NPROC]; 
+};
+
+extern struct lentry locks[];
+extern	int	nextlock;
+extern unsigned long ctr1000;
+#endif
