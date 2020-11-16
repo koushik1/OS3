a93ba088e482f0cbe2600ab89262c1305380da5b
Koushik <koushik1@users.noreply.github.com>
2020-11-16 15:13:16 -0500

    Create linit.c

---
+++ csc501-lab3/sys/linit.c
@@ -0,0 +1,33 @@
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+#include <q.h>
+#include <lock.h>
+#include <stdio.h>
+
+struct  lentry  locks[NLOCKS];
+int nextlock;
+
+
+void linit(){
+    struct lentry *lptr;
+    int i=0;
+    nextlock = NLOCKS - 1;
+
+    while(i < NLOCKS){
+
+        lptr = &locks[i];
+        lptr->lstate = LFREE;
+        lptr->lqhead = newqueue();
+        lptr->lqtail = 1 + lptr->lqhead;
+	    lptr->lprio  = -1;	
+
+        int j=0;
+        while(j<NPROC)
+        {
+            lptr->lproc_list[j] = 0;
+            j++;
+        }
+        i++;
+    }
+}
