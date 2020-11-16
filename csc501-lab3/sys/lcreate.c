654c2e39e3e5ac36764c8d4264a247fbcde30acf
Koushik <koushik1@users.noreply.github.com>
2020-11-16 15:12:20 -0500

    Create lcreate.c

---
+++ csc501-lab3/sys/lcreate.c
@@ -0,0 +1,37 @@
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+#include <q.h>
+#include <lock.h>
+#include <stdio.h>
+
+
+int lcreate()
+{
+	STATWORD ps;    
+	disable(ps);
+
+	int	lock_desc;
+    int i;
+    for (i - 0; i < NLOCKS; i++)
+    {
+        if (nextlock < 0)
+        {
+            nextlock = NLOCKS - 1;
+        }
+        lock_desc = nextlock;
+		if (locks[lock_desc].lstate==LFREE) {
+			locks[lock_desc].lstate = LUSED;
+            restore(ps);
+			return(lock_desc);
+		}
+        nextlock = nextlock - 1;
+
+    }
+    restore(ps);
+    return(SYSERR);
+
+
+}
+
+
