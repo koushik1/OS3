38dfd708d0d82adbd85eff44112dc39d1ab350d7
Koushik <koushik1@users.noreply.github.com>
2020-11-16 16:23:28 -0500

    Update lock.h

--- csc501-lab3/h/lock.h
+++ csc501-lab3/h/lock.h
@@ -1,24 +1,33 @@
+/* lock.h - isbadlock */
+
 #ifndef _LOCK_H_
 #define _LOCK_H_
 
-#define READ	1
-#define WRITE	2
+#ifndef NLOCKS
+#define	NLOCKS		50	/* number of maximum locks */
+#endif
+
+#define	LFREE	'\01'		/* this lock is free		*/
+#define	LUSED	'\02'		/* this lock is used		*/
+
+#ifndef DELETED
 #define DELETED -6
-#define NLOCKS 50
+#endif
 
-#define LFREE  0
-#define LUSED  1
+#define READ 0
+#define WRITE 1
 
-struct	lentry	{		
-	char lstate;		
-	int	lqhead;		
-	int	lqtail;		
-	int	ltype;	
-	int	lprio;		
-	int lproc_list[NPROC]; 
+struct	lentry	{		/* lock table entry			*/
+	char	lstate;		/* the state LFREE or LUSED		*/
+	int	lqhead;		/* q index of head of list		*/
+	int	lqtail;		/* q index of tail of list		*/
+	int	ltype;		/* lock type READ or WRITE		*/
+	int	lprio;		/* maximum priority among lock's wait queue */
+	int 	lproc_list[NPROC]; /* bit mask of process ids currently holding the lock  */
 };
-
-extern struct lentry locks[];
+extern	struct	lentry	rw_locks[];
 extern	int	nextlock;
 extern unsigned long ctr1000;
+#define	isbadlock(s)	(s<0 || s>=NLOCKS)
+
 #endif
